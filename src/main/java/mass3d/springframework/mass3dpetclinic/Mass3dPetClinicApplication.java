cf42f0fe8bad2e91b538c2cbf0f339d64d55fb4d
Hamza-ye <7amza.it@gmail.com>
2021-04-24 01:07:25 +0300

    Initial project add

---
+++ src/main/java/mass3d/springframework/mass3dpetclinic/Mass3dPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mass3d.springframework.mass3dpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mass3dPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mass3dPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mass3d/springframework/mass3dpetclinic/Mass3dPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mass3d.springframework.mass3dpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mass3dPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
