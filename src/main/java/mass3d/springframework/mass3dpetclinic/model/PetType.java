c48904a63af73125135df1a7a90924678c931296
Hamza-ye <7amza.it@gmail.com>
2021-04-25 00:51:38 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/mass3d/springframework/mass3dpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package mass3d.springframework.mass3dpetclinic.model;
+
+/**
+ * Created by Hamza on 25/04/2021.
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/mass3d/springframework/mass3dpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package mass3d.springframework.mass3dpetclinic.model;
+
+/**
+ * Created by Hamza on 25/04/2021.
+ */
+public class Vet extends Person {
+}
