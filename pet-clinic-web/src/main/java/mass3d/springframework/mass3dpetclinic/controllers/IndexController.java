7f4ba98a7112b23b7b5ef2a27cd3f12b42010499
Hamza-ye <7amza.it@gmail.com>
2021-04-26 04:12:47 +0300

    Add Pet Clinic Index Page and Controller. closes #8

---
+++ pet-clinic-web/src/main/java/mass3d/springframework/mass3dpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package mass3d.springframework.mass3dpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Hamza on 26/04/2021.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
