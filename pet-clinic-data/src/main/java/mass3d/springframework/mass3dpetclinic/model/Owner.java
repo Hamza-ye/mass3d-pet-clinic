5f13edb94aaafb4ab5f8b205277630865fc75e0a
Hamza-ye <7amza.it@gmail.com>
2021-04-29 00:04:23 +0300

    Create Pet Type, Pet, Visit entities. closes #27

--- pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package mass3d.springframework.mass3dpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Hamza on 25/04/2021.
  */
 public class Owner extends Person {
+
+    Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
