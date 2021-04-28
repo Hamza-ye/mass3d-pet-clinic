f8ed0b1f67f2363e3c8ce42c575b1a68c3062934
Hamza-ye <7amza.it@gmail.com>
2021-04-29 01:04:27 +0300

    Create Speciality Map Service impl. closes #32

---
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package mass3d.springframework.mass3dpetclinic.services;
+
+import mass3d.springframework.mass3dpetclinic.model.Specialty;
+
+/**
+ * Created by Hamza on 29/04/2021.
+ */
+public interface SpecialityService extends CrudService<Specialty, Long> {
+}
