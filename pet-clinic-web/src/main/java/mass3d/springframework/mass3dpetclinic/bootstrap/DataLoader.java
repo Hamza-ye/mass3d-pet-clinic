1aba29ec9adc39a5e177be8f2d0bfca2a4017cc9
Hamza-ye <7amza.it@gmail.com>
2021-04-29 00:30:32 +0300

    Add PetTypes in with Bootstrap. closes #33

--- pet-clinic-web/src/main/java/mass3d/springframework/mass3dpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mass3d/springframework/mass3dpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package mass3d.springframework.mass3dpetclinic.bootstrap;
 
 import mass3d.springframework.mass3dpetclinic.model.Owner;
+import mass3d.springframework.mass3dpetclinic.model.PetType;
 import mass3d.springframework.mass3dpetclinic.model.Vet;
 import mass3d.springframework.mass3dpetclinic.services.OwnerService;
+import mass3d.springframework.mass3dpetclinic.services.PetTypeService;
 import mass3d.springframework.mass3dpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,15 +17,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Owner1");
