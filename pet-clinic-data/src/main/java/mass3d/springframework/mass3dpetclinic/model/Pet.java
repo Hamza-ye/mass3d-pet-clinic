972379a615f30feaef2a4cc845bc8ed45e0fd7b6
Hamza-ye <7amza.it@gmail.com>
2021-04-30 02:27:27 +0300

    Create Visit Entity. closes #39

--- pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Pet.java
@@ -1,9 +1,9 @@
 package mass3d.springframework.mass3dpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Hamza on 25/04/2021.
@@ -22,6 +22,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -53,5 +56,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/model/Visit.java
@@ -1,14 +1,21 @@
 package mass3d.springframework.mass3dpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
 /**
  * Created by Hamza on 29/04/2021.
  */
+@Entity
 public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
