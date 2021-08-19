622d883ae161848febbdf163386e1193aee6f5e4
Hamza-ye <7amza.it@gmail.com>
2021-08-19 20:19:30 +0300

    Edit Abstract classes to implement CrudService

--- pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/services/map/AbstractMapService.java
@@ -1,24 +1,28 @@
 package mass3d.springframework.mass3dpetclinic.services.map;
 
 import mass3d.springframework.mass3dpetclinic.model.BaseEntity;
+import mass3d.springframework.mass3dpetclinic.services.CrudService;
 
 import java.util.*;
 
 /**
  * Created by Hamza on 26/04/2021.
  */
-public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+public class AbstractMapService<T extends BaseEntity, ID extends Long> implements CrudService<T, ID> {
 
     Map<Long , T> map = new HashMap<>();
 
+    @Override
     public Set<T> findAll(){
         return new HashSet<>(map.values());
     }
 
+    @Override
     public T findById(ID id){
         return map.get(id);
     }
 
+    @Override
     public T save(T object){
         if (object != null) {
             if (object.getId() == null) object.setId(getNextId());
@@ -29,10 +33,12 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
         return object;
     }
 
+    @Override
     public void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
+    @Override
     public void deleteById(ID id){
         map.remove(id);
     }
--- pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/services/springdatajpa/AbstractSDJpaService.java
+++ pet-clinic-data/src/main/java/mass3d/springframework/mass3dpetclinic/services/springdatajpa/AbstractSDJpaService.java
@@ -1,5 +1,6 @@
 package mass3d.springframework.mass3dpetclinic.services.springdatajpa;
 
+import mass3d.springframework.mass3dpetclinic.services.CrudService;
 import org.springframework.data.repository.CrudRepository;
 
 import java.io.Serializable;
@@ -9,7 +10,8 @@ import java.util.Set;
 /**
  * Created by Hamza on 30/04/2021.
  */
-public class AbstractSDJpaService <T, ID extends Serializable, R extends CrudRepository<T, ID>> {
+public class AbstractSDJpaService <T, ID extends Serializable, R extends CrudRepository<T, ID>>
+        implements CrudService<T, ID> {
 
     protected R repository;
 
@@ -17,24 +19,29 @@ public class AbstractSDJpaService <T, ID extends Serializable, R extends CrudRep
         this.repository = repository;
     }
 
+    @Override
     public Set<T> findAll() {
         Set<T> objects = new HashSet<>();
         repository.findAll().forEach(object -> objects.add(object));
         return objects;
     }
 
+    @Override
     public T findById(ID id) {
         return repository.findById(id).orElse(null);
     }
 
+    @Override
     public T save(T object) {
         return repository.save(object);
     }
 
+    @Override
     public void delete(T object) {
         repository.delete(object);
     }
 
+    @Override
     public void deleteById(ID id) {
         repository.deleteById(id);
     }
